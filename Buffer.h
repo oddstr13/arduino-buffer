1170ee20582e9c6437b1950cc67ad7220c3ebd7d
Odd Stråbø <oddstr13@openshell.no>
2019-01-28 14:22:50 +0100

    !BREAKING! Rework Buffer library.

--- Buffer.h
+++ Buffer.h
@@ -10,16 +10,6 @@ class Buffer {
         uint16_t ptr;
         uint16_t size;
         uint8_t *buf = NULL;
-        /*
-        void reset();
-        void addString(const char *value);
-        void addString(String value);
-        //void addString(const __FlashStringHelper *ifsh);
-        void addFloat(double value, uint8_t precission, bool strip);
-        void addCharArray(char *value, uint8_t len);
-        void addLong(unsigned long value);
-        void addWord(uint16_t value);
-        void addByte(uint8_t value);*/
 
         void setBuffer(uint8_t *buffer, uint16_t newsize) {
             size = newsize;
@@ -29,9 +19,13 @@ class Buffer {
         void reset() {
             ptr = 0;
         }
+        
+        void add(char value) { // byte, char, unsigned char
+            buf[ptr++] = value;
+        }
 
         // Add \0 terminated string, excluding \0
-        void addString(const char *value) {
+        void add(const char *value) {
             int i = 0;
             while (value[i]) {
                 if (ptr < size) {
@@ -40,58 +34,44 @@ class Buffer {
             }
         }
 
-        void addString(String value) {
-            addString(value.c_str());
+        void add(String value) {
+            add(value.c_str());
         }
 
-        void addFloat(double value, uint8_t precission=2, bool strip=true) {
-            #ifdef AVR
-            dtostrf(value, 1, precission, _floatbuf);
-            #else
-            snprintf(_floatbuf, 16, "%.*f", precission, value);
-            #endif
+        String _floatbuf;
+        void addNumber(double value, uint8_t precission=2, bool strip=true) {
+            // TODO: replace most of this code with String(), but keep strip functionality
+            _floatbuf = String(value, precission);
 
             if (precission and strip) {
-                uint8_t e = 0;
-                for (uint8_t i=0;i<16;i++) {
-                    if (!_floatbuf[i]) {
-                        e = i-1;
-                        break;
-                    }
-                }
-                for (uint8_t i=e; i; i--) {
-                    if (_floatbuf[i] == '0') {
-                        _floatbuf[i] = 0;
-                    } else if (_floatbuf[i] == '.') {
-                        _floatbuf[i] = 0;
+                for (uint8_t i=_floatbuf.length(); i; i--) {
+                    char c = _floatbuf.charAt(i);
+                    if (not c) {
+                        // Skip null bytes, just in case we should have started at length()-1
+                        // TODO: verify if this is needed, and remove this check if it isn't.
+                    } else if (c == '0') {
+                        _floatbuf.remove(i);
+                    } else if (c == '.') {
+                        _floatbuf.remove(i);
                         break;
                     } else {
                         break;
                     }
                 }
             }
-            for (uint8_t i=0; i<16;i++) {
-                if (_floatbuf[i] == 0) {
-                    break;
-                }
-                if (_floatbuf[i] >= 'A' and _floatbuf[i] <= 'Z') {
-                    _floatbuf[i] += 32; // str.tolower()
-                }
-            }
-            addString(_floatbuf);
+            _floatbuf.toLowerCase(); // NaN -> nan
+            add(_floatbuf);
         }
 
-        void addNumber(unsigned long long value) {
-            snprintf(_floatbuf, 16, "%u", value);
-            addString(_floatbuf);
+        void addNumber(unsigned long value, uint8_t base=DEC) {
+            add(String(value, base).c_str);
         }
 
-        void addSignedNumber(long long value) {
-            snprintf(_floatbuf, 16, "%d", value);
-            addString(_floatbuf);
+        void addNumber(long value, uint8_t base=DEC) {
+            add(String(value, base).c_str);
         }
 
-        void addCharArray(char *value, uint8_t len) {
+        void addNumber(char *value, uint8_t len) {
             // Add char array to the output data buffer (databuf) 
             for (uint8_t i=0; i<len; i++) {
                 if (ptr < size) {
@@ -100,19 +80,19 @@ class Buffer {
             }
         }
 
-        void addLong(unsigned long value) { // long, unsigned long
+        void addBytes(unsigned long value) { // long, unsigned long
             buf[ptr++] = value >> 24 & 0xff;
             buf[ptr++] = value >> 16 & 0xff;
             buf[ptr++] = value >> 8 & 0xff;
             buf[ptr++] = value & 0xff;
         }
 
-        void addWord(uint16_t value) { // word, int, unsigned int
+        void addBytes(uint16_t value) { // word, int, unsigned int
             buf[ptr++] = value >> 8 & 0xff;
             buf[ptr++] = value & 0xff;
         }
 
-        void addByte(uint8_t value) { // byte, char, unsigned char
+        void addBytes(uint8_t value) { // byte, char, unsigned char
             buf[ptr++] = value;
         }
 
